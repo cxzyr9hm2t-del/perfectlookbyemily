b0087eb6d1331cb007f7ad02f9e2e1b5b8abd168
cxzyr9hm2t-del <cxzyr9hm2t@privaterelay.appleid.com>
2026-03-12 11:55:54 -0400

    feat: configure next.config.js — output: export, distDir: firebase-deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-    images: {
-        unoptimized: true,
-          },
-          };
-          
-          module.exports = nextConfig;
+  output: 'export',
+  distDir: 'firebase-deploy',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
